47c760b0f20fda50ecd049999288718b244f97c3
aislinnennis <116472948+aislinnennis@users.noreply.github.com>
2024-04-18 09:18:33 -0400

    Create testlogical.v

---
+++ lab4a/testlogical.v
@@ -0,0 +1,14 @@
+module logical(A, B, OA, Y);
+
+  // inputs
+  input [7:0] A, B;
+  input OA;
+
+  // outputs
+  output [7:0] Y;
+
+  /* ADD YOUR CODE BELOW THIS LINE */
+
+  /* ADD YOUR CODE ABOVE THIS LINE */
+
+endmodule

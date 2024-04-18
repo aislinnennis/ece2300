a7ebe5b4b1253c1af0989d8fead3247e99c0c48f
aislinnennis <116472948+aislinnennis@users.noreply.github.com>
2024-04-18 09:12:44 -0400

    Create testadder.v

---
+++ lab4a/testadder.v
@@ -0,0 +1,16 @@
+module adder(A, B, CI, Y, C, V);
+
+  // inputs
+  input [7:0] A, B;
+  input CI;
+
+  // outputs
+  output [7:0] Y;
+  output C, V;
+
+  /* ADD YOUR CODE BELOW THIS LINE */
+
+  /* ADD YOUR CODE ABOVE THIS LINE */
+
+
+endmodule
